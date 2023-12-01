832701edf4ef14d489b8ff4f8a8bd3218e437004
ithuleyho <ithuleyho@proton.me>
2023-12-01 15:49:35 +0200

    Day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,17 +7,17 @@ trim = f . f
 
 filterNum xs = filter isDigit xs
 
-replace ('o':'n':'e':xs) = '1': replace ('e':xs)
-replace ('t':'w':'o':xs) = '2': replace ('o':xs)
+replace ('o':'n':'e':xs)         = '1': replace ('e':xs)
+replace ('t':'w':'o':xs)         = '2': replace ('o':xs)
 replace ('t':'h':'r':'e':'e':xs) = '3': replace ('e':xs)
-replace ('f':'o':'u':'r':xs) = '4': replace (xs)
-replace ('f':'i':'v':'e':xs) = '5': replace ('e':xs)
-replace ('s':'i':'x':xs) = '6': replace (xs)
-replace ('s':'e':'v':'e':'n':xs) = '7' : replace ('n':xs)
+replace ('f':'o':'u':'r':xs)     = '4': replace (xs)
+replace ('f':'i':'v':'e':xs)     = '5': replace ('e':xs)
+replace ('s':'i':'x':xs)         = '6': replace (xs)
+replace ('s':'e':'v':'e':'n':xs) = '7': replace ('n':xs)
 replace ('e':'i':'g':'h':'t':xs) = '8': replace ('t':xs)
-replace ('n':'i':'n':'e':xs) = '9': replace ('e':xs)
-replace (x:xs) = x : replace(xs)
-replace xs = xs
+replace ('n':'i':'n':'e':xs)     = '9': replace ('e':xs)
+replace (x:xs)                   = x : replace(xs)
+replace xs                       = xs
 
 part1 input = sum numbers2
   where inputlines   = lines (trim input)
