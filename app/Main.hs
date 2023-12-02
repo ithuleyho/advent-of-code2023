80269afb4df145426a807cffda7791d5abbfa787
ithuleyho <ithuleyho@proton.me>
2023-12-02 13:10:15 +0200

    Day 2 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,23 @@
 module Main (main) where
 
 import System.IO  
-import Control.Monad
-import Day01 (solve)
-filename = "day01-part1"
---filename = "test"
+import Data.Char
+
+import Day02 (solve)
+
+filename :: String
+filename = "day02-part1"
+-- filename = "test"
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
 
 main :: IO ()
 main = do  
-        let list = []
         handle <- openFile ("inputs/" ++ filename) ReadMode
         contents <- hGetContents handle
-        let result = solve contents
+        let result = solve $ trim contents
         print result
         hClose handle   
 
---
+++ src/Day02.hs
@@ -0,0 +1,42 @@
+module Day02 (solve) where
+
+import Data.Char
+import Debug.Trace
+
+gameNumber line = read gamenum :: Integer
+  where
+    dropped = drop 5 line
+    gamenum = takeWhile (\x -> x /= ':') dropped
+
+redMax = 12
+greenMax = 13
+blueMax = 14
+
+parseSets prev (' ' : xs) = parseSets prev xs
+parseSets prev (';' : xs) = parseSets prev xs
+parseSets prev (',' : xs) = parseSets prev xs
+parseSets prev ('r' : 'e' : 'd' : xs) = if prev > redMax 
+                                        then False 
+                                        else parseSets 0 xs
+parseSets prev ('g' : 'r' : 'e' : 'e' : 'n' : xs) = if prev > greenMax 
+                                                    then False 
+                                                    else parseSets 0 xs
+parseSets prev ('b' : 'l' : 'u' : 'e' : xs) = if prev > blueMax 
+                                              then False 
+                                              else parseSets 0 xs
+parseSets prev [] = True
+parseSets prev xs = parseSets num rest
+  where
+    digits = takeWhile isDigit xs
+    rest = drop (length digits) xs
+    num = read digits :: Integer
+
+parse line = if parseSets 0 sets then gameNumber line else 0
+  where
+    sets = drop 1 $ dropWhile (\x -> x /= ':') line
+
+part1 input = sum . map parse $ lines input
+
+part2 input = 0
+
+solve = part1
